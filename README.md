d9f57f55561984bd6d2ece8112bebdcb2b9ab333
denFete <dynamit1>
2015-11-06 19:59:21 +0100

    Complete!

---
+++ README.md
@@ -0,0 +1,4 @@
+### cv-fredrikcarlsson
+
+* Install dependencies using `npm install`
+* Set up local server with `grunt serve`. Server should run on port 9000
---
+++ js/custom.js
@@ -0,0 +1,38 @@
+ /* Custom modified js */
+
+ angular.module('cvApp', [])
+
+	.controller('portfolioCtrl', ['$scope', function($scope){
+
+		$scope.projects = [];
+
+		var NUM_PICTURES_CLOCKWORK = 9, NUM_KORTFILMER = 11, NUM_INVASION = 16, NUM_TUMBA_TARZAN = 4, NUM_FOTON = 23;
+
+		for(var i = 0; i < NUM_FOTON; i++){
+			var obj = {'title' : 'Övriga foton', 'img_prepath' : 'foton', 'img_format': 'jpg', 'img_index' : i+1};
+			$scope.projects.push(obj);
+		}
+
+		for(var i = 0; i < NUM_PICTURES_CLOCKWORK; i++){
+			var obj = {'title' : 'Clockwork Orange', 'img_prepath' : 'clockwork', 'img_format' : 'jpg', 'img_index' : i+1};
+			$scope.projects.push(obj);
+		}
+
+		for(var i = 0; i < NUM_INVASION; i++){
+			var obj = {'title' : 'Invasion', 'img_prepath' : 'invasion', 'img_format' : 'jpg', 'img_index' : i+1};
+			$scope.projects.push(obj);
+		}
+
+		// Första är png LOL på fidde
+		for(var i = 1; i < NUM_KORTFILMER; i++){
+			var obj = {'title' : 'Blandade kortfilmer', 'img_prepath' : 'kortfilmer', 'img_format' : 'jpg', 'img_index' : i+1};
+			$scope.projects.push(obj);
+		}
+
+		for(var i = 0; i < NUM_TUMBA_TARZAN; i++){
+			var obj = {'title' : 'Tumba Tarzan', 'img_prepath' : 'tumba_tarzan', 'img_format' : 'jpg', 'img_index' : i+1};
+			$scope.projects.push(obj);
+		}
+
+
+	}])
